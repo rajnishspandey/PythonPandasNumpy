aafd91d5f9b92e4afce4290f28faed02fa7d579e
rajnishspandey <pandeyrajnish86@yahoo.com>
2023-10-29 21:17:06 +0530

    NumPy

--- 07NumPy04.py
+++ 07NumPy04.py
@@ -1,7 +1,4 @@
-"""
-A useful function for plotting mathematical functions is linspace. 
-Linspace returns evenly spaced numbers over a specified interval
-"""
+import matplotlib.pyplot as plt
 import numpy as np
 
 print("We can create the following numpy array in Radians")
@@ -10,3 +7,39 @@ print(x)
 
 x = np.sin(x)
 print(x)
+
+"""
+#Linspace :
+A useful function for plotting mathematical functions is linspace. 
+Linspace returns evenly spaced numbers over a specified interval
+
+numpy.linspace(start, stop, num = int value)
+
+start : start of interval range
+
+stop : end of interval range
+
+num : Number of samples to generate.
+"""
+
+# Makeup a numpy array within [-2, 2] and 5 elements
+print("for 5 element")
+element = np.linspace(-2, 2, num=5)
+print(element)
+
+# Make a numpy array within [-2, 2] and 9 elements
+print("for 9 element")
+element  = np.linspace(-2, 2, num=9)
+print(element)
+
+print("# Make a numpy array within [0, 2π] and 100 elements" )
+x = np.linspace(0, 2*np.pi, num=100)
+print(x)
+
+print("# Calculate the sine of x list")
+y = np.sin(x)
+print(y)
+
+plt.figure("Testing")
+plt.plot(x,y)
+plt.show()
